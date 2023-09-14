342ec966bfa44d9deec9b05d177024084ae7aaa0
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 03:04:48 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 from quickstart import Drive_OCR
+import time
 db=Drive_OCR("").google_spreadsheet_get("13Aw2HghuOauGAjnxgD5YvBYo7Ysda-TprTJ_BLHuIPA","A:N")
 
-st.write(1234)
+st.set_page_config()
+
+ph = st.empty()
+N = len(db)*30
+for secs in range(N,0,-1):
+    mm, ss = secs//60, secs%60
+    ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
+    time.sleep(1)
+
 for x in db:
 	z=[]
 	for y in x[1:-1]:
