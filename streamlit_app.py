93ec5255e9f0ff4e9a9f2b025934905f8e14e1c3
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 02:29:17 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("python -m venv .venv & pip install streamlit")
-os.system("python aa.py")
-#os.system("/home/appuser/venv/bin/python aa.py")
+#os.system("python -m venv .venv & pip install streamlit")
+#os.system("python aa.py")
+os.system("/home/adminuser/venv/bin/python aa.py")
