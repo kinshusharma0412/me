cf0d7ecce86ac7122cd9230421acb42386d40e4a
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 03:49:11 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import time
 db=Drive_OCR("").google_spreadsheet_get("13Aw2HghuOauGAjnxgD5YvBYo7Ysda-TprTJ_BLHuIPA","A:N")
 
 st.set_page_config()
+query=st.experimental_get_query_params()
 
 ph = st.empty()
 N = len(db)*30
@@ -19,13 +20,12 @@ for x in db:
 	st.selectbox(x[0],z)
 
 st.write("Thanks")
-@st.cache_data
-def timer(N):
-	for secs in range(N,0,-1):
+st.write(query)
+for secs in range(st.session_state["timer"],0,-1):
 		mm, ss = secs//60, secs%60
 		ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
 		time.sleep(1)
 		st.session_state["timer"]=secs-2
 
-timer(st.session_state["timer"])
+timer()
     
