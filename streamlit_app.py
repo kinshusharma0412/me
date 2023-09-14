4b01aa19d8e3f78a96237f36c53c565cc33f7be7
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 02:47:23 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,22 @@
-import os
+import streamlit as st
+from quickstart import Drive_OCR
+db=Drive_OCR("").google_spreadsheet_get("13Aw2HghuOauGAjnxgD5YvBYo7Ysda-TprTJ_BLHuIPA","A:N")
+
+st.write(1234)
+for x in db:
+	z=[]
+	for y in x[1:-1]:
+		if y!="":
+			z.append(y)
+	st.selectbox(x[0],z)
+#submitted = st.form_submit_button("Submit")
+#if submitted:
+#st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Thanks")
+        
+#import os
 
 #os.system("python -m venv .venv & pip install streamlit")
 #os.system("python aa.py")
-os.system("/home/adminuser/venv/bin/python aa.py")
+#os.system("/home/adminuser/venv/bin/python aa.py")
