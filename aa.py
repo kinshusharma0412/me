a65c1290013c5f3cc516a8bf65b00202bef60c8f
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 02:35:28 +0530

    speed se push kiya me Folder me

--- aa.py
+++ aa.py
@@ -3,14 +3,14 @@
 import streamlit as st
 from quickstart import Drive_OCR
 db=Drive_OCR("").google_spreadsheet_get("13Aw2HghuOauGAjnxgD5YvBYo7Ysda-TprTJ_BLHuIPA","A:N")
-print(db)
-st.write("hello")
+
+st.write(1234)
 for x in db:
 	z=[]
 	for y in x[1:-1]:
 		if y!="":
 			z.append(y)
-	print(st.selectbox(x[0],z))
+	st.selectbox(x[0],z)
 #submitted = st.form_submit_button("Submit")
 #if submitted:
 #st.write("slider", slider_val, "checkbox", checkbox_val)
