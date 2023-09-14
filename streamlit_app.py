e464f35563ea82a58ec6e32a04cc0516a076e587
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 04:05:07 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,16 @@ from quickstart import Drive_OCR
 import time
 query=st.experimental_get_query_params()
 #db=Drive_OCR("").google_spreadsheet_get("13Aw2HghuOauGAjnxgD5YvBYo7Ysda-TprTJ_BLHuIPA","A:N")
-print(query["id"][0])
-db=Drive_OCR("").google_spreadsheet_get(query["id"][0],"A:N")
+print(query)
+db=Drive_OCR("").google_spreadsheet_get(query["id"],"A:N")
 
 st.set_page_config()
 
 
 ph = st.empty()
 N = len(db)*30
-if query["id"][0]+query["user"] not in st.session_state:
-	st.session_state[query["id"][0]+query["user"]]=N
+if query["id"]+query["user"] not in st.session_state:
+	st.session_state[query["id"]+query["user"]]=N
 
 	
 for x in db:
@@ -24,12 +24,12 @@ for x in db:
 
 st.write("Thanks")
 st.write(query)
-if st.session_state[query["id"][0]+query["user"]]>0:
-	for secs in range(st.session_state[query["id"][0]+query["user"]],-1,-1):
+if st.session_state[query["id"]+query["user"]]>0:
+	for secs in range(st.session_state[query["id"]+query["user"]],-1,-1):
 		mm, ss = secs//60, secs%60
 		ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
 		time.sleep(1)
-		st.session_state[query["id"][0]+query["user"]]=secs-2
+		st.session_state[query["id"]+query["user"]]=secs-2
 		if secs<1:
 			ph.metric("Times Up!!")
 
