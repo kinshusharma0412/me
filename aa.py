1bf5d9367faece18cfc0be2959282db634a71c53
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 01:53:56 +0530

    speed se push kiya me Folder me

--- aa.py
+++ aa.py
@@ -1,4 +1,4 @@
-import streamlit as st
+from home.adminuser.venv.bin import streamlit as st
 from quickstart import Drive_OCR
 db=Drive_OCR("").google_spreadsheet_get("13Aw2HghuOauGAjnxgD5YvBYo7Ysda-TprTJ_BLHuIPA","A:N")
 print(db)
