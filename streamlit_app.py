eac39ead6b0c1d07f8d8cef8abc824c9a081c3e1
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 13:51:39 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ for x in db:
 	btn.append(st.selectbox(x[0],z))
 
 st.write("Thanks")
-st.write(query["id"][0])
+
 
 db2=Drive_OCR("").google_spreadsheet_get(query["id"][0],"Sheet2!A:"+cell2)
 ids=[]
@@ -47,6 +47,7 @@ else:
 			break
 		i+=1
 	db2[i]=["Name",query["user"][0],int(os.environ[query["id"][0]+query["user"][0]])].extend(btn)
+	st.write(ids)
 	st.write(db2)
 	Drive_OCR("").google_spreadsheet_update(query["id"][0],"Sheet2!A:"+cell2, "USER_ENTERED",db2)
 	
