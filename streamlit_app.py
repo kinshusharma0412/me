e2e679d371d3fc39e5c3baa3c4b3230dbb731188
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-15 13:11:04 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from quickstart import Drive_OCR
-import time,os
+import time,os, string,xlsxwriter
 query=st.experimental_get_query_params()
 #db=Drive_OCR("").google_spreadsheet_get("13Aw2HghuOauGAjnxgD5YvBYo7Ysda-TprTJ_BLHuIPA","A:N")
 print(query)
-db=Drive_OCR("").google_spreadsheet_get(query["id"][0],"A:N")
+db=Drive_OCR("").google_spreadsheet_get(query["id"][0],"Sheet1!A:N")
+ 
+cell2=xlsxwriter.utility.xl_col_to_name(len(db)+3)
 
 st.set_page_config()
 
@@ -36,6 +38,16 @@ if int(os.environ[query["id"][0]+query["user"][0]])>0:
 			ph.write(btn)
 else:
 	ph.write(btn)
+db2=Drive_OCR("").google_spreadsheet_get(query["id"][0],"Sheet2!A:"+cell2)
+ids=[]
+for x in db2:
+	ids.append(x[1])
 
+data=["Name",query["user"][0]]
+for x in btn:
+	data.append(btn)
+if query["user"] is not in ids:
+	Drive_OCR("").google_spreadsheet_update("1UlOdBum1_aolah0grnTfmmWobElvf8O7Un9-Z_aekYQ","Sheet2!A:"+cell2, "USER_ENTERED",data)
+		
 
     
