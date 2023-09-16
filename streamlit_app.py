022dce6cfe1302f3b7b04eb3806774b264337242
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-16 20:52:03 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            a.embeddedAppMetaInfoBar_linkOutText__4ptMa {visibility: hidden;}
+            div.embeddedAppMetaInfoBar_container__DxxL1 {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
