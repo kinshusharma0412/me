a419fb24b6af3d21915490f222ed574ee08df755
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-16 15:30:49 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -217,9 +217,9 @@ elif int(sub[i])==4:
 	def cooling_highlight(val):
 		color = 'green' if val["Sr."]==indi else 'white'
 		return f'background-color: {color}'*4
-	df = pd.DataFrame(tab,columns=["Sr.","Name","Time","Marks"],index=False)
+	df = pd.DataFrame(tab,columns=["Sr.","Name","Time","Marks"])
 	
-	#df.style.hide(axis="index")
+	df.style.hide(axis="index").format(escape="html")
 	df.style.apply(cooling_highlight)
 	st.table(df)
 	
