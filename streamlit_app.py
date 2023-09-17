223818973ba17e1cbdc365a8c5e2c5d5bb7c11e5
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-18 01:34:27 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -211,7 +211,7 @@ elif int(sub[i])==3:
 		st.experimental_rerun()
 	
 	for x in range(len(db)):
-		st.write(":red[Q"+str(x+1)+". "+db[x][0]+"]")
+		st.write(":golden[Q"+str(x+1)+". "+db[x][0]+"]")
 		tt=""
 		for y in range(len(db[x][1:-3])):
 			if db[x][y+1]=="":
