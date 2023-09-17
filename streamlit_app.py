bf40f8059f2eab9ff9eb40d01c259eca6ca5ebd8
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-17 16:03:43 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,17 +77,17 @@ if int(sub[i])==0:
 		for y in range(len(db[x][1:-3])):
 			if db[x][1:-3][y]!="" :
 				temp2=""
-				try:
-					if query["user"][0] not in os.environ:
-						temp2=st.button(db[x][1:-3][y],key=str(x+1)+"."+str(y+1),on_click=click_button,args=[str(x+1)+"."+str(y+1)])
-					elif str(os.environ[query["user"][0]][str(x+1)])==str(y+1):
-						temp2=st.button("```      ```:green["+db[x][1:-3][y]+"]",key=str(x+1)+"."+str(y+1),on_click=click_button,args=[str(x+1)+"."+str(y+1)])
-					else:
-						temp2=st.button(db[x][1:-3][y],key=str(x+1)+"."+str(y+1),on_click=click_button,args=[str(x+1)+"."+str(y+1)])
+				
+				if query["user"][0] not in os.environ:
+					temp2=st.button(db[x][1:-3][y],key=str(x+1)+"."+str(y+1),on_click=click_button,args=[str(x+1)+"."+str(y+1)])
+				elif str(os.environ[query["user"][0]][str(x+1)])==str(y+1):
+					temp2=st.button("```      ```:green["+db[x][1:-3][y]+"]",key=str(x+1)+"."+str(y+1),on_click=click_button,args=[str(x+1)+"."+str(y+1)])
+				else:
+					temp2=st.button(db[x][1:-3][y],key=str(x+1)+"."+str(y+1),on_click=click_button,args=[str(x+1)+"."+str(y+1)])
 					
-				except Exception as e:
+				
 					
-					st.warning(str(x+1)+"."+str(y+1)+str(e), icon="⚠️")
+					#st.warning(str(x+1)+"."+str(y+1)+str(e), icon="⚠️")
 					#temp2=st.button(db[x][1:-3][y],key=str(x+1)+"."+str(y+1))
 				temp.append(temp2)
 		btn2.append(temp)
