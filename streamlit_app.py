c5e707be95715230e58d9df9406f9c7c445ef34b
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-17 20:00:22 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,10 +62,11 @@ if int(sub[i])==0:
 		y=reaaa.split("\.",i)
 		temp3[y[0]]=y[1]
 		os.environ[query["user"][0]+query["id"][0]]=str(temp3)
-		if len(db2[i])-4>int(y[0])-1:
+		
+		_="""if len(db2[i])-4>int(y[0])-1:
 			db2[i][4+int(y)-1]=y[1]
 		elif len(db2[i])-4=int(y[0])-1:
-			db2[i].append(y[1])
+			db2[i].append(y[1])"""
 		
 
 
