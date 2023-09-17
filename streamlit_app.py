e21c9a03fc69c02e8a231f40fa3d2101eb89de25
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-17 18:15:42 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,7 +113,7 @@ if int(sub[i])==0:
 			try:
 				db2[i][4+x]=ast.literal_eval(os.environ[query["user"][0]+query["id"][0]])[str(1+x)]
 			except Exception as e:
-				db2[i][4+x]=""
+				db2[i].append("")
 		Drive_OCR("").google_spreadsheet_update(query["id"][0],"Sheet2!A:"+cell2, "USER_ENTERED",db2)
 		st.experimental_rerun()
 		
@@ -154,7 +154,7 @@ if int(sub[i])==0:
 					try:
 						db2[i][4+x]=ast.literal_eval(os.environ[query["user"][0]+query["id"][0]])[str(1+x)]
 					except Exception as e:
-						db2[i][4+x]=""
+						db2[i].append("")
 				Drive_OCR("").google_spreadsheet_update(query["id"][0],"Sheet2!A:"+cell2, "USER_ENTERED",db2)
 				st.experimental_rerun()
 	else:
