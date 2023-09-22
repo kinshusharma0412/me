e8dfa54e4994f9a5f60ed18039320eb0b967d81e
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-22 18:08:32 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,10 @@ if query["id"][0] not in os.environ:
 
 db=[]
 dbb=[]
-yi=reaaa.split("\], \[",reaaa.sub("^\[\[|\]\]$","",os.environ[query["id"][0]]))
+yi=reaaa.split("\], \[",(os.environ[query["id"][0]]))
 for x in yi:
-	dbb.append(str(reaaa.split("', '",reaaa.sub("^\'|\'$","",x))))
+	x
+	dbb.append(str(reaaa.split("', '",(x))))
 db.append(dbb)
 
 cell2=xlsxwriter.utility.xl_col_to_name(len(db)+3)
