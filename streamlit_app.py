eac7376ae2e7c849ce0a902a5e8056bce3d1a14d
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-22 10:38:10 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -218,7 +218,7 @@ elif int(sub[i])==3:
 		Drive_OCR("").google_spreadsheet_update(query["id"][0],"Sheet2!A:"+cell2, "USER_ENTERED",db2)
 		st.experimental_rerun()
 	df=csv.reader(open('./data/Quiz.csv', 'r') )
-	st.write((df))
+	st.write(list(df))
 	from PIL import Image
 #	image = Image.open('sunrise.jpg')
 #	st.image(image, caption='Sunrise by the mountains')
