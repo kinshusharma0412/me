5fb793fc35d96b59a3526082f3e7571552f6eee0
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-22 12:21:43 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,8 +256,11 @@ elif int(sub[i])==3:
 						tt+="```      ```:green["+db[x][y+1]+"]```      ```+4  \n"
 					else:
 						tt+="```      ```:red["+db[x][y+1]+"]```      ```-1  \n"
+				elif int(y)+1==int(db[x][-1]):
+					tt+="```      ```:green["+db[x][y+1]+"]  \n"
 				else:
-					st.write("db2[i][x+4]="+str(db2[i][x+4]))
+					tt+="```   ```"+db[x][y+1]+"  \n"
+					
 			elif int(y)+1==int(db[x][-1]):
 				tt+="```      ```:green["+db[x][y+1]+"]  \n"
 			else:
