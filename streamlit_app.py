96575a7be045629c0c8e8a1148ab72d0c53252d0
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-09-28 12:16:25 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ from PIL import Image
 
 @st.cache_resource
 def init_connection():
-	return MongoClient('mongodb+srv://'+st.secrets["username"]+':'+quote_plus(st.secrets["password"])+'@cluster0.uo8sfvz.mongodb.net/?retryWrites=true&w=majority')
+	return MongoClient('mongodb+srv://'+st.secrets.db_mango["username"]+':'+quote_plus(st.secrets.db_mango["password"])+'@cluster0.uo8sfvz.mongodb.net/?retryWrites=true&w=majority')
 cm = init_connection()
 
 hide_streamlit_style = """
