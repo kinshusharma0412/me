2fc79ddf18a4c48fdffa2f2ee4870b9cee1727be
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-05 19:54:01 +0530

    speed se push kiya me Folder me

--- PDF.py
+++ PDF.py
@@ -12,19 +12,24 @@ import site,glob
 import os
 
 import streamlit as st
+
+
+import os, sys
+
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
 from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
-
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-features=NetworkService")
-options.add_argument("--window-size=1920x1080")
-options.add_argument("--disable-features=VizDisplayCompositor")
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+browser = webdriver.Firefox(options=opts)
 def delete_selenium_log():
     if os.path.exists('selenium.log'):
         os.remove('selenium.log')
