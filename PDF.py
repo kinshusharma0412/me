dc007cd0f98570b1388e362954854a1af8e32c13
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-08 22:59:32 +0530

    speed se push kiya me Folder me

--- PDF.py
+++ PDF.py
@@ -31,7 +31,7 @@ options.add_argument("--disable-features=VizDisplayCompositor")
 @st.experimental_singleton
 def installff():
   os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.11/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
 _ = installff()
 from selenium import webdriver
