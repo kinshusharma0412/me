e146dccd92681fd9d79e318784901dee205e6658
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-08 15:55:16 +0530

    speed se push kiya me Folder me

--- PDF.py
+++ PDF.py
@@ -14,22 +14,18 @@ import streamlit as st
 import os, sys
 with st.echo():
     from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
 
-    @st.experimental_singleton
     def get_driver():
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-
-    driver = get_driver()
-    driver.get("http://example.com")
-
-    st.code(driver.page_source)
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()) )
+    
+    run = st.button('run')
+    
+    if run:
+        driver = get_driver()        
+        driver.set_window_size(1400, 1000)
+        driver.get("https://discuss.streamlit.io/")
 
 site.addsitedir(r"...pathToPDFTron\PDFNetWrappersWin32\PDFNetC\Lib")
 from PDFNetPython3 import PDFDoc, Optimizer, SDFDoc
