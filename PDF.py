5d84a093af08e78816f689e296c28e2a3b4c0d79
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-08 15:44:54 +0530

    speed se push kiya me Folder me

--- PDF.py
+++ PDF.py
@@ -10,43 +10,26 @@ import pdfkit
 import pandas as pd
 import site,glob
 import os
-
 import streamlit as st
-
-
 import os, sys
+with st.echo():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
 
-import streamlit as st
+    @st.experimental_singleton
+    def get_driver():
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-"""
-## Web scraping on Streamlit Cloud with Selenium
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
 
-[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
+    driver = get_driver()
+    driver.get("http://example.com")
 
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
-
-Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-"""
-
-import streamlit as st
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-@st.cache_resource
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
-
-driver = get_driver()
-driver.get('http://example.com')
-
-st.code(driver.page_source)
+    st.code(driver.page_source)
 
 site.addsitedir(r"...pathToPDFTron\PDFNetWrappersWin32\PDFNetC\Lib")
 from PDFNetPython3 import PDFDoc, Optimizer, SDFDoc
