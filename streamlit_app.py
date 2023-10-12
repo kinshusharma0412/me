37e56f49b72fc713dd5afef66fda7ad574390cc0
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-12 16:53:41 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,11 +75,11 @@ hide_streamlit_style = """
 
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 @st.cache_resource
-def dbbb(data):
+def Reloading_Question_Bank(data):
 	return cm["Live_Quiz"]["db"].find_one({"db":{"$type":"object"}})["db"][data]
 
 try:
-	db=dbbb(query["id"][0])
+	db=Reloading_Question_Bank(query["id"][0])
 except:
 	st.write("please open our site only in @soojhboojh_01bot in telegram")
 	st.stop()
