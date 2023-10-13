a16d1d4cb84c198004b5d1318805baa3b25e48a9
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-13 08:30:19 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -769,7 +769,7 @@ elif int(os.environ[query["id"][0]+query["user"][0]+"s"])==4:
 
 		ti=f"{mm:02d}:{ss:02d}"
 
-		tab.append([new_result2[x]["Sr."]+". ",'<a target="_self" href="?name='+quote_plus(new_result2[x]["Name"])+'&user='+new_result2[x]["id"]+'www.google.com'+'">'+new_result2[x]["Name"]+'</a>',ti,new_result2[x]["Marks"]])
+		tab.append([new_result2[x]["Sr."]+". ",'<a target="_top" href="?name='+quote_plus(new_result2[x]["Name"])+'&user='+new_result2[x]["id"]+'">'+new_result2[x]["Name"]+'</a>',ti,new_result2[x]["Marks"]])
 		
 
 	
