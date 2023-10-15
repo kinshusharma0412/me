c6bc86e57a7f79c9c511dc3bed6d5d427b28958e
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-16 03:55:21 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,11 +188,11 @@ elif int(os.environ[query["id"][0]+query["user"][0]+"s"])==0:
 			for y in range(5):
 				try:
 					if str(ast.literal_eval(os.environ[query["user"][0]+query["id"][0]])[str(counter)])!="0":
-						ttt+='<button type="button" style="width: 20%; background-color: green;" on_click="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
+						ttt+='<button type="button" style="width: 20%; background-color: green;" onclick="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
 					else:
-						ttt+='<button type="button" style="width: 20%; background-color: red;" on_click="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
+						ttt+='<button type="button" style="width: 20%; background-color: red;" onclick="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
 				except:
-					ttt+='<button type="button" style="width: 20%; background-color: white;" on_click="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
+					ttt+='<button type="button" style="width: 20%; background-color: white;" onclick="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
 					
 				
 				
@@ -205,11 +205,11 @@ elif int(os.environ[query["id"][0]+query["user"][0]+"s"])==0:
 			if counter<=len(db):
 				try:
 					if str(ast.literal_eval(os.environ[query["user"][0]+query["id"][0]])[str(counter)])!="0":
-						ttt+='<button type="button" style="width: 20%; background-color: green;" on_click="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
+						ttt+='<button type="button" style="width: 20%; background-color: green;" onclick="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
 					else:
-						ttt+='<button type="button" style="width: 20%; background-color: red;" on_click="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
+						ttt+='<button type="button" style="width: 20%; background-color: red;" onclick="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
 				except:
-					ttt+='<button type="button" style="width: 20%; background-color: white;" on_click="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
+					ttt+='<button type="button" style="width: 20%; background-color: white;" onclick="queCounter('+str(counter)+')">'+str(counter).zfill(len(str(len(db))))+'</button>'
 			counter+=1
 		java="""
 <script>
