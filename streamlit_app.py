f473a5d58bdcddcd1f89eac27b506af63262891c
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-16 10:16:02 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -223,7 +223,10 @@ elif int(os.environ[query["id"][0]+query["user"][0]+"s"])==0:
 function queCounter(n_element) {
 var buttons = window.parent.document.getElementsByClassName("stMarkdown");
 var button = buttons[n_element];
-button.scrollIntoView({ behavior: "smooth"});
+window.scroll({
+  top: button+30,
+  behavior: 'smooth'
+});
 }"""
 		my_html = f"""<script>{my_js}</script>"""
 		if len(db)%5==0:
