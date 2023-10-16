ca56d1383df02f4c71e555670a65d0b82e731bbc
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-16 10:08:29 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -226,7 +226,7 @@ var button = buttons[n_element];
 button.scrollIntoView({ behavior: "smooth"});
 }"""
 		my_html = f"""<script>{my_js}</script>"""
-		st.components.v1.html(ttt+my_html,height=500,scrolling=True)
+		st.components.v1.html(ttt+my_html,height=10,scrolling=True)
 		
 
 	for x in range(len(db)):
