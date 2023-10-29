0a008b5cd2229ec77ecb12969d9ff973192fcd87
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-10-29 12:52:28 +0530

    speed se push kiya me Folder me

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ except:
 	st.stop()
 try:
 	N=len(db)*int(query["n"][0])
-	st.write(str(N))
+	st.write(str(query["n"][0]))
 except:
 	N=len(db)*50
 if query["id"][0]+query["user"][0]+"sc" not in os.environ:
