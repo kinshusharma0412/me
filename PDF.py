b88a58a764325994c551098228d764b963343e98
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-11-03 20:29:48 +0530

    speed se push kiya me Folder me

--- PDF.py
+++ PDF.py
@@ -279,10 +279,8 @@ elif database.toggle('Database'):
 			option = st.selectbox('Select a database name ',(cm[st.session_state.list_database].list_collection_names()))
 			st.session_state.list_collection=option
 		else:
-			option = st.selectbox('Select a database name ',(cm.list_database_names()))
-			if option:
-				st.session_state.list_database=option
-			option = st.selectbox('Select a database name ',(cm[st.session_state.list_database].list_collection_names()))
+			
+			option = st.selectbox('Select a database name ',(cm[st.session_state.list_database].list_collection_names()),placeholder=st.session_state.list_database)
 			if option:
 				st.session_state.list_database=option
 			st.write(cm[st.session_state.list_database][st.session_state.list_collection])
