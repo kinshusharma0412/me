ff83c87f7cfb2234042268123bd0f8bd06558f1f
kinshusharm0412 <kinshusharm0412@gmail.com>
2023-11-04 08:12:18 +0530

    speed se push kiya me Folder me

--- PDF.py
+++ PDF.py
@@ -311,7 +311,7 @@ elif database.toggle('Database'):
 		if "select_database" in st.session_state:
 			xyz=st.write(cm[new[st.session_state.list_database][0]][new[st.session_state.list_database][1]].find_one({'_id':document_names[st.session_state.select_database-1]}))
 			xxx=type(cm[new[st.session_state.list_database][0]][new[st.session_state.list_database][1]].find_one({'_id':document_names[st.session_state.select_database-1]}))
-			
-			options = st.multiselect(xxx,['Green', 'Yellow', 'Red', 'Blue'],['Yellow', 'Red'])
+			st.write(xxx)
+			options = st.multiselect("testing?",['Green', 'Yellow', 'Red', 'Blue'],['Yellow', 'Red'])
 			st.write('You selected:', options)
 		
